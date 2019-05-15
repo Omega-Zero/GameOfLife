f227522a4c2b3d0785755ea2b8bf35321b626f40
Omega-Zero <36581543+Omega-Zero@users.noreply.github.com>
2019-05-15 13:27:11 -0400

    Frame setup and useful imports

--- src/GameOfLifeGrid.java
+++ src/GameOfLifeGrid.java
@@ -1,11 +1,24 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class GameOfLifeGrid {
 
 
 
 public static void main(String[] args) { 
+	
+	// Creates the main window
+    JFrame mainWindow = new JFrame("The Game of Life");
+    mainWindow.setSize(730, 715);
+    mainWindow.setVisible(true);
+    
 
-   
+
+
+  
 } 
+
+
 
 }
