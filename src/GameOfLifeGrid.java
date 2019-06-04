07c1271712a78b482aa78057218111ab8c0184d7
Omega-Zero <36581543+Omega-Zero@users.noreply.github.com>
2019-06-04 12:25:49 -0400

    StdDraw library saved in directory

--- src/GameOfLifeGrid.java
+++ src/GameOfLifeGrid.java
@@ -1,6 +1,6 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
+
 
 public class GameOfLifeGrid {
 
@@ -10,10 +10,12 @@ public static void main(String[] args) {
 	
 	// Creates the main window
     JFrame mainWindow = new JFrame("The Game of Life");
-    mainWindow.setSize(730, 715);
+    mainWindow.setSize(750, 750);
     mainWindow.setVisible(true);
+   
+    JLabel gridLabel = new JLabel();
+    StdDraw.square(0.2, 0.8, 0.1);
     
-
 
 
   
