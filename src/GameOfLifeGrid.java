f7066462f602c27c1d5a21949fa00a08c303c8b4
Omega-Zero <36581543+Omega-Zero@users.noreply.github.com>
2019-06-06 16:13:14 -0400

    now draws a grid on the screen using a double for loop

--- src/GameOfLifeGrid.java
+++ src/GameOfLifeGrid.java
@@ -6,25 +6,33 @@ import javax.swing.JLabel;
 
 public class GameOfLifeGrid {
 
-int arraySize = 10; 
-boolean initialArray[][] = new boolean[arraySize][arraySize]; 
-boolean newArray[][] = new boolean[arraySize][arraySize]; 
+static int arraySize = 10; 
+static int column = arraySize;
+static int row = arraySize;
+static boolean initialArray[][] = new boolean[column][row]; 
+static boolean newArray[][] = new boolean[column][row]; 
 
 
 public static void main(String[] args) { 
 	
-	// Creates the main window
-    JFrame mainWindow = new JFrame("The Game of Life");
-    mainWindow.setSize(750, 750);
-    mainWindow.setVisible(true);
-   
-    StdDraw.square(0.2, 0.8, 0.1);
-    
-    
+	drawGrid();
     
 
   
 } 
+
+//Draws a blank grid
+public static void drawGrid() {
+	
+	for(double j=0.00; j<arraySize; j++) {
+		   for (double i=0.00; i<arraySize; i++) {
+			   double arraySizeDouble = new Double(arraySize);
+			   double xCoord = i / arraySizeDouble;
+			   double yCoord = j / arraySizeDouble;
+			   StdDraw.square(xCoord, yCoord, 0.5);
+		   }
+	 } 
+}
 
 
 
--- src/StdDraw.java
+++ src/StdDraw.java
@@ -1,5 +1,5 @@
 //This class can be found at https://introcs.cs.princeton.edu/java/stdlib/StdDraw.java.html
-
+//This is a modified version to fit my GoL needs
 /******************************************************************************
  *  Compilation:  javac StdDraw.java
  *  Execution:    java StdDraw
@@ -306,7 +306,7 @@ public final class StdDraw implements ActionListener, MouseListener, MouseMotion
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);            // closes all windows
         // frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);      // closes only current window
-        frame.setTitle("Standard Draw");
+        frame.setTitle("Game of Life");
         frame.setJMenuBar(createMenuBar());
         frame.pack();
         frame.requestFocusInWindow();
@@ -1668,26 +1668,26 @@ public final class StdDraw implements ActionListener, MouseListener, MouseMotion
      * @param args the command-line arguments
      */
     public static void main(String[] args) {
-        StdDraw.square(0.2, 0.8, 0.1);
-        StdDraw.filledSquare(0.8, 0.8, 0.2);
-        StdDraw.circle(0.8, 0.2, 0.2);
-
-        StdDraw.setPenColor(StdDraw.BOOK_RED);
-        StdDraw.setPenRadius(0.02);
-        StdDraw.arc(0.8, 0.2, 0.1, 200, 45);
-
-        // draw a blue diamond
-        StdDraw.setPenRadius();
-        StdDraw.setPenColor(StdDraw.BOOK_BLUE);
-        double[] x = { 0.1, 0.2, 0.3, 0.2 };
-        double[] y = { 0.2, 0.3, 0.2, 0.1 };
-        StdDraw.filledPolygon(x, y);
-
-        // text
-        StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.text(0.2, 0.5, "black text");
-        StdDraw.setPenColor(StdDraw.WHITE);
-        StdDraw.text(0.8, 0.8, "white text");
+//        StdDraw.square(0.2, 0.8, 0.1);
+//        StdDraw.filledSquare(0.8, 0.8, 0.2);
+//        StdDraw.circle(0.8, 0.2, 0.2);
+//
+//        StdDraw.setPenColor(StdDraw.BOOK_RED);
+//        StdDraw.setPenRadius(0.02);
+//        StdDraw.arc(0.8, 0.2, 0.1, 200, 45);
+//
+//        // draw a blue diamond
+//        StdDraw.setPenRadius();
+//        StdDraw.setPenColor(StdDraw.BOOK_BLUE);
+//        double[] x = { 0.1, 0.2, 0.3, 0.2 };
+//        double[] y = { 0.2, 0.3, 0.2, 0.1 };
+//        StdDraw.filledPolygon(x, y);
+//
+//        // text
+//        StdDraw.setPenColor(StdDraw.BLACK);
+//        StdDraw.text(0.2, 0.5, "black text");
+//        StdDraw.setPenColor(StdDraw.WHITE);
+//        StdDraw.text(0.8, 0.8, "white text");
     }
 
 }
