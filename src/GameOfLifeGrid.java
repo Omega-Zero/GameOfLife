abcec8ad01576bba4d47e5f407211d8c097cfda2
Omega-Zero <36581543+Omega-Zero@users.noreply.github.com>
2019-06-12 14:40:36 -0400

    variable name changes//method changing

--- src/GameOfLifeGrid.java
+++ src/GameOfLifeGrid.java
@@ -6,57 +6,58 @@ import javax.swing.JLabel;
 
 public class GameOfLifeGrid {
 
-static int arraySize = 20; 
-static int column = arraySize;
-static int row = arraySize;
-static boolean initialArray[][] = new boolean[column][row]; 
-static boolean newArray[][] = new boolean[column][row]; 
+
 
 
 public static void main(String[] args) { 
 	
-	//drawGrid();
-reDrawGrid();
+	final int COLUMNS = 20;
+	final int ROWS = 20;
+	boolean initialArray[][] = new boolean[COLUMNS][ROWS]; 
+	boolean newArray[][] = new boolean[COLUMNS][ROWS]; 
+	boolean swap[][] = new boolean[COLUMNS][ROWS];
+	
+	//Checks arrays to see boolean values and draw appropriately 
+	for(int column=1; column<COLUMNS; column++) {
+		   for (int row=1; row<ROWS; row++) {
+			   double xCoord = ((double)column / COLUMNS);
+			   double yCoord = ((double)row / ROWS);
+			   StdDraw.square(xCoord, yCoord, .1);
+		   }
+	}
+}			   
+			   
 //0	StdDraw.square(.025, .025, .025);
 //1	StdDraw.square(.025, .075, .025);
 //2	StdDraw.square(.025, .125, .025);
 //3	StdDraw.square(.025, .175, .025);
 //4   StdDraw.square(.025, .225, .025);
-} 
 
 //Draws a blank grid
-public static void drawGrid() {
-	
-	for(double j=1.00; j<arraySize+1; j++) {
-		   for (double i=1.00; i<arraySize+1; i++) {
-			   double arraySizeDouble = new Double(arraySize);
-			   double xCoord = (i / arraySizeDouble) - 1;
-			   double yCoord = (j / arraySizeDouble) - 1;
-			   StdDraw.square(xCoord, yCoord, 1.0);
-		   }
-	 } 
-}
+
 
 //Draws grid with any true array indexes being a filled square
-public static void reDrawGrid() {
-	   newArray[1][3] = true;
-for(double j=1.00; j<(double)arraySize+1; j++) {
-for (double i=1.00; i<(double)arraySize+1; i++) {
-	   double arraySizeDouble = new Double(arraySize) + 1.00;
-	   double xCoord = (i / arraySizeDouble) - 1;
-	   double yCoord = (j / arraySizeDouble) - 1;
-	   int xInt = (int)i;
-	   int yInt = (int)j;
-				   if(newArray[xInt][yInt] == false) {
-					   StdDraw.square(xCoord, yCoord, 1.0);
-     			   }else{
-					   
-					  
-					   StdDraw.filledSquare(xCoord, yCoord, 1.0);
-				   }
-				   
-			   }
-		}
-	}
+//public static void reDrawGrid() {
+//	   newArray[1][3] = true;
+//for(int j=0; j<arraySize; j++) {
+//for (int i=0; i<arraySize; i++) {
+//
+//	   double xCoord = i / arraySize;
+//	   double yCoord = j / arraySize;
+//	   int xInt = (int)i;
+//	   int yInt = (int)j;
+//				   if(newArray[xInt][yInt] == false) {
+//					   StdDraw.square(xCoord, yCoord, 1.0);
+//     			   }else{
+//					   
+//					  
+//					   StdDraw.filledSquare(xCoord, yCoord, 1.0);
+//				   }
+//				   
+//			   }
+//		}
+//	}
+//
+
 
 }
