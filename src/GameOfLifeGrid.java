bc86c2424f9b25d41b29f697f66d3ec8ec030a96
Omega-Zero <36581543+Omega-Zero@users.noreply.github.com>
2019-07-05 16:03:49 -0400

    bounds check updated; some generation logic working now

--- src/GameOfLifeGrid.java
+++ src/GameOfLifeGrid.java
@@ -1,193 +1,183 @@
+
 //Jeffrey's Java Implenetation of the Game of Life using StdDraw class methods
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-
 public class GameOfLifeGrid {
 
-
-
-
-public static void main(String[] args) { 
+	final static int COLUMNS = 20;
+	final static int ROWS = 20;
+	final static int GENERATIONS = 500;
+	static boolean initialArray[][] = new boolean[COLUMNS][ROWS];
+	static boolean newArray[][] = new boolean[COLUMNS][ROWS];
+	static boolean swapArray[][];
 	
-	final int COLUMNS = 20;
-	final int ROWS = 20;
-	final int GENERATIONS = 500;
-	
-	boolean initialArray[][] = new boolean[COLUMNS][ROWS]; 
-	boolean newArray[][] = new boolean[COLUMNS][ROWS]; 
-	boolean emptyArray[][] = new boolean[COLUMNS][ROWS];
-	
-	initialArray[5][1] = true;
-	initialArray[6][1] = true;
-	newArray = initialArray;
-//	StdDraw.square(.025, .025, .025);
-	//StdDraw.square(.025, .075, .025);
-	//Checks arrays to see boolean values and draw appropriately 
-	for(int column=0; column<COLUMNS; column++) {
-		   for (int row=0; row<ROWS; row++) {
-			   double xCoord = ((double)column * .05) + .025;
-			   double yCoord = ((double)row * .05) + .025;
-			   
-			   	if(initialArray[column][row]) {
-			   		StdDraw.filledSquare(xCoord, yCoord, .025);
-			   	}else {
-			   		StdDraw.square(xCoord, yCoord, .025);
-			   	}
-			 
-		   }
-	}
-	
-	//Actual Life Logic
-	
-int totalNeighbors = 0;	 
-for (int generation = 0; generation < GENERATIONS; generation++) {
-	
-	for(int column=0; column<COLUMNS; column++) {
-		   for (int row=0; row<ROWS; row++) {
-			   
-			  
-			   
-			   //If cell is alive
-			   if(initialArray[column][row]) {
-				   
-				   //array bounds check
-				   if(column-1 >= 0 && row -1 >=0 && column+1 < COLUMNS && row+1 < ROWS) {
-					
-				   //Check all its neighbors and tick variable appropriately 
-					  totalNeighbors = 0;
-				      if(initialArray[column-1][row-1]) {
-						   totalNeighbors = totalNeighbors + 1;
-					   }
-					   if(initialArray[column][row-1]) {
-						   totalNeighbors = totalNeighbors + 1;					   
-					   }
-					   if(initialArray[column+1][row-1]) {
-						   totalNeighbors = totalNeighbors + 1;
-					   }
-					   if(initialArray[column-1][row]) {
-						   totalNeighbors = totalNeighbors + 1;
-					   }
-					   if(initialArray[column+1][row]) {
-						   totalNeighbors = totalNeighbors + 1;
-					   }
-					   if(initialArray[column-1][row+1]) {
-						   totalNeighbors = totalNeighbors + 1;
-					   }
-					   if(initialArray[column][row+1]) {
-						   totalNeighbors = totalNeighbors + 1;					   
-					   }
-					   if(initialArray[column+1][row+1]) {
-						   totalNeighbors = totalNeighbors + 1;
-				       }
-					
-				   //After neighbor checks, if cell has exactly 3 of neighbors...
-					if (totalNeighbors <= 1) {
-						newArray[column][row] = false;
-					}else if(totalNeighbors >= 4) {
-						newArray[column][row] = false;
-					}else {
-						newArray[column][row] = true;
-					}
-				   }
-				//Else the cell is dead..
-			    }else {
-			    	totalNeighbors = 0;
-			    	//if row/column are in bounds
-			    	 if(column-1 >= 0 && row -1 >=0 && column+1 < COLUMNS && row+1 < ROWS) {
-			    		  //check neighbors
-			    		  if(initialArray[column-1][row-1]) {
-							   totalNeighbors = totalNeighbors + 1;
-						   }
-						   if(initialArray[column][row-1]) {
-							   totalNeighbors = totalNeighbors + 1;					   
-						   }
-						   if(initialArray[column+1][row-1]) {
-							   totalNeighbors = totalNeighbors + 1;
-						   }
-						   if(initialArray[column-1][row]) {
-							   totalNeighbors = totalNeighbors + 1;
-						   }
-						   if(initialArray[column+1][row]) {
-							   totalNeighbors = totalNeighbors + 1;
-						   }
-						   if(initialArray[column-1][row+1]) {
-							   totalNeighbors = totalNeighbors + 1;
-						   }
-						   if(initialArray[column][row+1]) {
-							   totalNeighbors = totalNeighbors + 1;					   
-						   }
-						   if(initialArray[column+1][row+1]) {
-							   totalNeighbors = totalNeighbors + 1;
-					       }
-			    	}
-					//If cell has 3 neighbors it comes to life else remains dead
-					if (totalNeighbors == 3) {
-						newArray[column][row] = true;
-					}else{
-						newArray[column][row] = false;
-					}
+	public static void main(String[] args) {
 		
-			    }
-			  
-		   }
-		   
-	  }
-	
-	//redraw array from newArray after every generation
-	for(int column=0; column<COLUMNS; column++) {
-		   for (int row=0; row<ROWS; row++) {
-			   double xCoord = ((double)column * .05) + .025;
-			   double yCoord = ((double)row * .05) + .025;
-			   	if(newArray[column][row]) {
-			   		StdDraw.filledSquare(xCoord, yCoord, .025);
-			   	}else{
-			   		StdDraw.square(xCoord, yCoord, .025);
-			   	}
-			 
-		   }
+		
+		initialArray[5][1] = true;
+		initialArray[6][1] = true;
+		
+		initialArray[17][5] = true;
+		initialArray[18][5] = true;
+		initialArray[16][5] = true;
+		initialArray[18][6] = true;
+		
+		initialArray[15][12] = true;
+		
+		drawGrid(initialArray);
+		
+		runLifeLogic();
+		
+		
+		
+
+//	newArray = initialArray;
+//
+//	drawGrid(newArray);
+
 	}
-	initialArray = newArray;
-	newArray = emptyArray;
+
+//Makes calls to StdDraw to appropriately draw grid based off array booleans 
+	public static void drawGrid(boolean[][] array) {
+
+		for (int column = 0; column < COLUMNS; column++) {
+			for (int row = 0; row < ROWS; row++) {
+				double xCoord = ((double) column * .05) + .025;
+				double yCoord = ((double) row * .05) + .025;
+				if (array[column][row] == true) {
+					StdDraw.filledSquare(xCoord, yCoord, .025);
+				} else {
+					StdDraw.square(xCoord, yCoord, .025);
+				}
+				
+			}
+		}
+	}//END drawGrid()
 	
-}
-
-}
-}
-			   
 	
 
+//Actual Life Logic
+	public static void runLifeLogic() {
 
-//0	StdDraw.square(.025, .025, .025); 
-//1	StdDraw.square(.025, .075, .025);
-//2	StdDraw.square(.025, .125, .025);
-//3	StdDraw.square(.025, .175, .025);
-//4   StdDraw.square(.025, .225, .025);
+		int totalNeighbors = 0;
 
-//Draws a blank grid
+		for (int generation = 0; generation < GENERATIONS; generation++) {
+
+			for (int column = 0; column < COLUMNS; column++) {
+
+				for (int row = 0; row < ROWS; row++) {
+
+					// If cell is alive
+					if (initialArray[column][row]) {
+
+						// array bounds check
+						if (column - 1 != -1 && row - 1 != -1 && column + 1 != COLUMNS && row + 1 != ROWS) {
+
+							// Check all its neighbors and tick variable appropriately
+							totalNeighbors = 0;
+							if (initialArray[column - 1][row - 1]) {
+								totalNeighbors = totalNeighbors + 1;
+							}
+							if (initialArray[column][row - 1]) {
+								totalNeighbors = totalNeighbors + 1;
+							}
+							if (initialArray[column + 1][row - 1]) {
+								totalNeighbors = totalNeighbors + 1;
+							}
+							if (initialArray[column - 1][row]) {
+								totalNeighbors = totalNeighbors + 1;
+							}
+							if (initialArray[column + 1][row]) {
+								totalNeighbors = totalNeighbors + 1;
+							}
+							if (initialArray[column - 1][row + 1]) {
+								totalNeighbors = totalNeighbors + 1;
+							}
+							if (initialArray[column][row + 1]) {
+								totalNeighbors = totalNeighbors + 1;
+							}
+							if (initialArray[column + 1][row + 1]) {
+								totalNeighbors = totalNeighbors + 1;
+							}
+						
+							// After neighbor checks, if cell has exactly 3 of neighbors...
+							if (totalNeighbors <= 1) {
+								newArray[column][row] = false;
+								totalNeighbors = 0;
+							} else if (totalNeighbors >= 4) {
+								newArray[column][row] = false;
+								totalNeighbors = 0;
+							} else {
+								newArray[column][row] = true;
+								totalNeighbors = 0;
+							}
+						}//END LIVE CELL BOUNDSCHECK
+							
+						// Else the cell is dead..
+						}else {
+							totalNeighbors = 0;
+							// if row/column are in bounds
+							if (column - 1 != -1 && row - 1 != -1 && column + 1 != COLUMNS && row + 1 != ROWS) {
+								// check neighbors
+								if (initialArray[column - 1][row - 1]) {
+									totalNeighbors = totalNeighbors + 1;
+								}
+								if (initialArray[column][row - 1]) {
+									totalNeighbors = totalNeighbors + 1;
+								}
+								if (initialArray[column + 1][row - 1]) {
+									totalNeighbors = totalNeighbors + 1;
+								}
+								if (initialArray[column - 1][row]) {
+									totalNeighbors = totalNeighbors + 1;
+								}
+								if (initialArray[column + 1][row]) {
+									totalNeighbors = totalNeighbors + 1;
+								}
+								if (initialArray[column - 1][row + 1]) {
+									totalNeighbors = totalNeighbors + 1;
+								}
+								if (initialArray[column][row + 1]) {
+									totalNeighbors = totalNeighbors + 1;
+								}
+								if (initialArray[column + 1][row + 1]) {
+									totalNeighbors = totalNeighbors + 1;
+								}
+
+								// If cell has 3 neighbors it comes to life else remains dead
+								if (totalNeighbors == 3) {
+									newArray[column][row] = true;
+									totalNeighbors = 0;
+								} else {
+									newArray[column][row] = false;
+									totalNeighbors = 0;
+
+								}
+
+							} // END DEADCELL BOUNDS CHECK
+
+						}//END LIVE/DEAD IF CHECK
+
+					}//END ROW LOOP
+
+				} // END COLUMN LOOP
+			
+			
+			swapArray = initialArray;
+			initialArray = newArray;
+			newArray = swapArray;
+			drawGrid(newArray);
+			System.out.println(initialArray[5][1]);
+			
+			} // END GEN  LOOP
+
+//redraw array from newArray after every generation
+			
+			
 
 
-//Draws grid with any true array indexes being a filled square
-//public static void reDrawGrid() {
-//	   newArray[1][3] = true;
-//for(int j=0; j<arraySize; j++) {
-//for (int i=0; i<arraySize; i++) {
-//
-//	   double xCoord = i / arraySize;
-//	   double yCoord = j / arraySize;
-//	   int xInt = (int)i;
-//	   int yInt = (int)j;
-//				   if(newArray[xInt][yInt] == false) {
-//					   StdDraw.square(xCoord, yCoord, 1.0);
-//     			   }else{
-//					   
-//					  
-//					   StdDraw.filledSquare(xCoord, yCoord, 1.0);
-//				   }
-//				   
-//			   }
-//		}
-//	}
-//
+	}// END METHOD runLifeLogic()
 
+}// END CLASS GameOfLifeGrid
