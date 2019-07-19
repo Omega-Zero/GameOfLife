b82ad250c64905d088d8acc5c9aa1c0afdfe60f5
Omega-Zero <36581543+Omega-Zero@users.noreply.github.com>
2019-07-19 16:40:54 -0400

    buffer time added for smoother gen. variables updated

--- src/GameOfLifeGrid.java
+++ src/GameOfLifeGrid.java
@@ -1,7 +1,6 @@
 
 //Jeffrey's Java Implenetation of the Game of Life using StdDraw class methods
 
-
 public class GameOfLifeGrid {
 
 	final static int COLUMNS = 20;
@@ -26,16 +25,14 @@ public class GameOfLifeGrid {
 		drawGrid(initialArray);
 
 		runLifeLogic();
-
-//	newArray = initialArray;
-//
-//	drawGrid(newArray);
-
 	}
 
 //Makes calls to StdDraw to appropriately draw grid based off array booleans 
 	public static void drawGrid(boolean[][] array) {
-
+		
+		StdDraw.clear();
+		StdDraw.enableDoubleBuffering();
+		
 		for (int column = 0; column < COLUMNS; column++) {
 			for (int row = 0; row < ROWS; row++) {
 				double xCoord = ((double) column * .05) + .025;
@@ -48,89 +45,115 @@ public class GameOfLifeGrid {
 
 			}
 		}
+		
+		StdDraw.show();
+
 	}// END drawGrid()
 
 //Actual Life Logic
 	public static void runLifeLogic() {
-
 
 		for (int generation = 0; generation < GENERATIONS; generation++) {
 
 			for (int column = 0; column < COLUMNS; column++) {
 
 				for (int row = 0; row < ROWS; row++) {
-
+					
+					int cellNeighborCount = neighborCheck(column, row);
+					
 					// If cell is alive
 					if (initialArray[column][row]) {
-
-						// array bounds check
-							
-							//doesCellDie
-							if(neighborCheck(column, row) == 1 || neighborCheck(column, row) == 0 || neighborCheck(column, row) >= 4 ) {
-								newArray[column][row] = false;
-							}else {
-								newArray[column][row] = true;
-							}
-							
-							
 						
-						
-						// Else the cell is dead..
+						// doesCellDie
+						if (cellNeighborCount == 1 || cellNeighborCount == 0 || cellNeighborCount >= 4) {
+							newArray[column][row] = false;
 						}else {
+							newArray[column][row] = true;
+						}
 
-								// doesCellComeToLife
-								if (neighborCheck(column, row) == 3) {
-									newArray[column][row] = true;
-								} else {
-									newArray[column][row] = false;
-								}
+						// Else the cell is dead..
+					} else {
 
-							} // END ELSE 
+						// doesCellComeToLife
+						if (cellNeighborCount == 3) {
+							newArray[column][row] = true;
+						}else {
+							newArray[column][row] = false;
+						}
 
-					}//END ROW LOOP
+					} // END ELSE
 
-				} // END COLUMN LOOP
+				} // END ROW LOOP
+
+			} // END COLUMN LOOP
+
+			swapArray = newArray;
+			newArray = initialArray;
+			initialArray = swapArray;
 			drawGrid(newArray);
-			swapArray = initialArray;
-			initialArray = newArray;
-			newArray = swapArray;
-				
-			} // END GEN LOOP
+
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} // END GEN LOOP
 	}
 
 	public static int neighborCheck(int column, int row) {
-		int totalNeighbors = 0;
-		if (column - 1 != -1 && row - 1 != -1 && column + 1 != COLUMNS && row + 1 != ROWS) {
-			
-			if (initialArray[column - 1][row - 1]) {
-				totalNeighbors = totalNeighbors + 1;
-			}
-			
-			if (initialArray[column][row - 1]) {
-				totalNeighbors = totalNeighbors + 1;
-			}
-			if (initialArray[column + 1][row - 1]) {
-				totalNeighbors = totalNeighbors + 1;
-			}
-			if (initialArray[column - 1][row]) {
-				totalNeighbors = totalNeighbors + 1;
-			}
-			if (initialArray[column + 1][row]) {
-				totalNeighbors = totalNeighbors + 1;
-			}
-			if (initialArray[column - 1][row + 1]) {
-				totalNeighbors = totalNeighbors + 1;
-			}
-			if (initialArray[column][row + 1]) {
-				totalNeighbors = totalNeighbors + 1;
-			}
-			if (initialArray[column + 1][row + 1]) {
-				totalNeighbors = totalNeighbors + 1;
-			}
-			
-		}
-		return totalNeighbors;
 		
+		int totalNeighbors = 0;
+
+			if (column - 1 != -1 && row - 1 != -1) {
+				if (initialArray[column - 1][row - 1]) {
+					totalNeighbors = totalNeighbors + 1;
+				}
+			}
+
+			if (row - 1 != -1) {
+				if (initialArray[column][row - 1]) {
+					totalNeighbors = totalNeighbors + 1;
+				}
+			}
+
+			if (column + 1 != COLUMNS && row - 1 != -1) {
+				if (initialArray[column + 1][row - 1]) {
+					totalNeighbors = totalNeighbors + 1;
+				}
+			}
+
+			if (column - 1 != -1) {
+				if (initialArray[column - 1][row]) {
+					totalNeighbors = totalNeighbors + 1;
+				}
+			}
+
+			if (column + 1 != COLUMNS) {
+				if (initialArray[column + 1][row]) {
+					totalNeighbors = totalNeighbors + 1;
+				}
+			}
+
+			if (column - 1 != -1 && row + 1 != ROWS) {
+				if (initialArray[column - 1][row + 1]) {
+					totalNeighbors = totalNeighbors + 1;
+				}
+			}
+
+			if (row + 1 != ROWS) {
+				if (initialArray[column][row + 1]) {
+					totalNeighbors = totalNeighbors + 1;
+				}
+			}
+
+			if (column + 1 != COLUMNS && row + 1 != ROWS) {
+				if (initialArray[column + 1][row + 1]) {
+					totalNeighbors = totalNeighbors + 1;
+				}
+			}
+		return totalNeighbors;
 	}
-	
-	}// END CLASS GameOfLifeGrid
+
+}// END CLASS GameOfLifeGrid
