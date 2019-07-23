f05583dfe7c7e9baaf120ec67825a8ebca31d0b6
Omega-Zero <36581543+Omega-Zero@users.noreply.github.com>
2019-07-23 11:07:21 -0400

    cleaning up static variables/adding method params

--- src/GameOfLifeGrid.java
+++ src/GameOfLifeGrid.java
@@ -3,45 +3,48 @@
 
 public class GameOfLifeGrid {
 
-	final static int COLUMNS = 20;
-	final static int ROWS = 20;
 	final static int GENERATIONS = 500;
-	static boolean initialArray[][] = new boolean[COLUMNS][ROWS];
-	static boolean newArray[][] = new boolean[COLUMNS][ROWS];
-	static boolean swapArray[][];
 
 	public static void main(String[] args) {
 
-		//Exploder Tester
-		initialArray[5][1] = true;
-		initialArray[6][1] = true;
-		initialArray[7][1] = true;
-		initialArray[8][1] = true;
-		initialArray[9][1] = true;
-		initialArray[10][1] = true;
-		initialArray[11][1] = true;
-		
-		//Simple test
-		initialArray[6][1] = true;
-		initialArray[17][5] = true;
-		initialArray[18][5] = true;
-		initialArray[16][5] = true;
-		initialArray[18][6] = true;
-		
-		//single cell test
-		initialArray[15][12] = true;
+		// Exploder Tester
+//		initialArray[5][1] = true;
+//		initialArray[6][1] = true;
+//		initialArray[7][1] = true;
+//		initialArray[8][1] = true;
+//		initialArray[9][1] = true;
+//		initialArray[10][1] = true;
+//		initialArray[11][1] = true;
 
-		drawGrid(initialArray);
+		// Simple test
+//		initialArray[6][1] = true;
+//		initialArray[17][5] = true;
+//		initialArray[18][5] = true;
+//		initialArray[16][5] = true;
+//		initialArray[18][6] = true;
 
-		runLifeLogic();
+		// single cell test
+//		initialArray[15][12] = true;
+		letThereBeLife();
+	}
+
+	public static void letThereBeLife() {
+		int COLUMNS = 20;
+		int ROWS = 20;
+		
+		boolean initialArray[][] = new boolean[COLUMNS][ROWS];
+		boolean newArray[][] = new boolean[COLUMNS][ROWS];
+		boolean swapArray[][] = new boolean[COLUMNS][ROWS];
+
+		runLifeLogic(initialArray, newArray, swapArray, COLUMNS, ROWS);
 	}
 
 //Makes calls to StdDraw to appropriately draw grid based off array booleans 
-	public static void drawGrid(boolean[][] array) {
-		
+	public static void drawGrid(boolean[][] array, int COLUMNS, int ROWS) {
+
 		StdDraw.clear();
 		StdDraw.enableDoubleBuffering();
-		
+
 		for (int column = 0; column < COLUMNS; column++) {
 			for (int row = 0; row < ROWS; row++) {
 				double xCoord = ((double) column * .05) + .025;
@@ -54,29 +57,31 @@ public class GameOfLifeGrid {
 
 			}
 		}
-		
+
 		StdDraw.show();
 
 	}// END drawGrid()
 
 //Actual Life Logic
-	public static void runLifeLogic() {
+	public static void runLifeLogic(boolean[][] initialArray, boolean[][] newArray, boolean[][] swapArray, int COLUMNS, int ROWS) {
+
+
 
 		for (int generation = 0; generation < GENERATIONS; generation++) {
 
 			for (int column = 0; column < COLUMNS; column++) {
 
 				for (int row = 0; row < ROWS; row++) {
-					
-					int cellNeighborCount = neighborCheck(column, row);
-					
+
+					int cellNeighborCount = neighborCheck(initialArray, column, COLUMNS, row, ROWS);
+
 					// If cell is alive
 					if (initialArray[column][row]) {
-						
+
 						// doesCellDie
 						if (cellNeighborCount == 1 || cellNeighborCount == 0 || cellNeighborCount >= 4) {
 							newArray[column][row] = false;
-						}else {
+						} else {
 							newArray[column][row] = true;
 						}
 
@@ -86,7 +91,7 @@ public class GameOfLifeGrid {
 						// doesCellComeToLife
 						if (cellNeighborCount == 3) {
 							newArray[column][row] = true;
-						}else {
+						} else {
 							newArray[column][row] = false;
 						}
 
@@ -99,71 +104,70 @@ public class GameOfLifeGrid {
 			swapArray = newArray;
 			newArray = initialArray;
 			initialArray = swapArray;
-			drawGrid(newArray);
+			drawGrid(newArray, COLUMNS, ROWS);
 
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();			
-			}//END try
+				e.printStackTrace();
+			} // END try
 
-			
 		} // END GEN LOOP
-		
-	}//END runLifeLogic();
 
-	public static int neighborCheck(int column, int row) {
-		
+	}// END runLifeLogic();
+
+	public static int neighborCheck(boolean[][] initialArray, int column, int COLUMNS, int row, int ROWS) {
+
 		int totalNeighbors = 0;
 
-			if (column - 1 != -1 && row - 1 != -1) {
-				if (initialArray[column - 1][row - 1]) {
-					totalNeighbors = totalNeighbors + 1;
-				}
+		if (column - 1 != -1 && row - 1 != -1) {
+			if (initialArray[column - 1][row - 1]) {
+				totalNeighbors = totalNeighbors + 1;
 			}
+		}
 
-			if (row - 1 != -1) {
-				if (initialArray[column][row - 1]) {
-					totalNeighbors = totalNeighbors + 1;
-				}
+		if (row - 1 != -1) {
+			if (initialArray[column][row - 1]) {
+				totalNeighbors = totalNeighbors + 1;
 			}
+		}
 
-			if (column + 1 != COLUMNS && row - 1 != -1) {
-				if (initialArray[column + 1][row - 1]) {
-					totalNeighbors = totalNeighbors + 1;
-				}
+		if (column + 1 != COLUMNS && row - 1 != -1) {
+			if (initialArray[column + 1][row - 1]) {
+				totalNeighbors = totalNeighbors + 1;
 			}
+		}
 
-			if (column - 1 != -1) {
-				if (initialArray[column - 1][row]) {
-					totalNeighbors = totalNeighbors + 1;
-				}
+		if (column - 1 != -1) {
+			if (initialArray[column - 1][row]) {
+				totalNeighbors = totalNeighbors + 1;
 			}
+		}
 
-			if (column + 1 != COLUMNS) {
-				if (initialArray[column + 1][row]) {
-					totalNeighbors = totalNeighbors + 1;
-				}
+		if (column + 1 != COLUMNS) {
+			if (initialArray[column + 1][row]) {
+				totalNeighbors = totalNeighbors + 1;
 			}
+		}
 
-			if (column - 1 != -1 && row + 1 != ROWS) {
-				if (initialArray[column - 1][row + 1]) {
-					totalNeighbors = totalNeighbors + 1;
-				}
+		if (column - 1 != -1 && row + 1 != ROWS) {
+			if (initialArray[column - 1][row + 1]) {
+				totalNeighbors = totalNeighbors + 1;
 			}
+		}
 
-			if (row + 1 != ROWS) {
-				if (initialArray[column][row + 1]) {
-					totalNeighbors = totalNeighbors + 1;
-				}
+		if (row + 1 != ROWS) {
+			if (initialArray[column][row + 1]) {
+				totalNeighbors = totalNeighbors + 1;
 			}
+		}
 
-			if (column + 1 != COLUMNS && row + 1 != ROWS) {
-				if (initialArray[column + 1][row + 1]) {
-					totalNeighbors = totalNeighbors + 1;
-				}
+		if (column + 1 != COLUMNS && row + 1 != ROWS) {
+			if (initialArray[column + 1][row + 1]) {
+				totalNeighbors = totalNeighbors + 1;
 			}
+		}
 		return totalNeighbors;
 	}
 
